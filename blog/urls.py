8a975a9d1018a352c5125a8374992b1f2b95125e
Nate Jones <jonesnl@umich.edu>
2015-04-10 13:15:04 -0400

    Remove unneccessary parts from repo

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-from blog import views
-
-urlpatterns = patterns('',
-                       url(r'^(?P<post_id>\d+)', views.post),
-                       url(r'^page/(?P<page_num>\d+)', views.latest_posts),
-                       url(r'^$', views.latest_posts, name='blog'),
-)
--- frontpage/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from frontpage.models import Data
-
-__author__ = 'nate'
-
-
-class DataAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Data, DataAdmin)
